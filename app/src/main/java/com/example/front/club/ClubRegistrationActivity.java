405d9f6f5e64f89f2c961f70c9e93201cd77b86f
nw9233fr2864 <nw9233fr2864@gmail.com>
2024-11-21 08:05:13 +0900

    feat: button frame

--- app/src/main/java/com/example/front/club/ClubRegistrationActivity.java
+++ app/src/main/java/com/example/front/club/ClubRegistrationActivity.java
@@ -1,8 +1,11 @@
 package com.example.front.club;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.front.R;
 
 public class ClubRegistrationActivity extends AppCompatActivity {
+    private EditText etClubRegistration;
+    private Button btnClubRegistrationDone;
+    private boolean isEtFilled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +30,34 @@ public class ClubRegistrationActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        etClubRegistration = findViewById(R.id.et_club_registration);
+        btnClubRegistrationDone = findViewById(R.id.btn_club_registration_done);
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            isEtFilled = !etClubRegistration.getText().toString().trim().isEmpty();
+            checkAllFields();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    };
+
+    private void checkAllFields() {
+        if (isEtFilled) {
+            btnClubRegistrationDone.setEnabled(true);
+        } else {
+            btnClubRegistrationDone.setEnabled(false);
+        }
     }
 }
