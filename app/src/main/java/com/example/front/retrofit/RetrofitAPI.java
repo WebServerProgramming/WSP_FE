afce35384b86be13679e48475a4bd5c1dc5cfea2
nw9233fr2864 <nw9233fr2864@gmail.com>
2024-11-21 07:48:53 +0900

    feat: retrofit frame

---
+++ app/src/main/java/com/example/front/retrofit/RetrofitAPI.java
@@ -0,0 +1,11 @@
+package com.example.front.retrofit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RetrofitAPI {
+    @GET("/oauth2/authorization/kakao")
+    Call<TokenResponse> getTokens(@Query("accessToken") String accessToken,
+                                  @Query("refreshToken") String refreshToken);
+}
---
+++ app/src/main/java/com/example/front/retrofit/RetrofitClientInstance.java
@@ -0,0 +1,34 @@
+package com.example.front.retrofit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://chawoomi.link/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            // Logging Interceptor 추가
+            HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(logging)
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static RetrofitAPI getApiService() {
+        return getRetrofitInstance().create(RetrofitAPI.class);
+    }
+}
