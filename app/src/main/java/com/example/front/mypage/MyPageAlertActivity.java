0f802fe66cc46b61a0984722ec0a49cfa779aefc
nw9233fr2864 <nw9233fr2864@gmail.com>
2024-12-10 21:21:52 +0900

    feat: mypage alert to save configuration

--- app/src/main/java/com/example/front/mypage/MyPageAlertActivity.java
+++ app/src/main/java/com/example/front/mypage/MyPageAlertActivity.java
@@ -1,8 +1,11 @@
 package com.example.front.mypage;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +20,28 @@ public class MyPageAlertActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_mypage_alert);
 
+        RadioGroup radioGroupNew = findViewById(R.id.rg_mypage_alert_new);
+        RadioGroup radioGroupRemind = findViewById(R.id.rg_mypage_alert_remind);
+        RadioButton rbNewOn = findViewById(R.id.rb_mypage_alert_new_on);
+        RadioButton rbNewOff = findViewById(R.id.rb_mypage_alarm_new_off);
+        RadioButton rbRemindOn = findViewById(R.id.rb_mypage_alert_remind_on);
+        RadioButton rbRemindOff = findViewById(R.id.rb_mypage_alert_remind_off);
+
+        String alertNew = getSharedPreferences("alert", Context.MODE_PRIVATE)
+                .getString("alertNew", "on");
+        String alertRemind = getSharedPreferences("alert", Context.MODE_PRIVATE)
+                .getString("alertRemind", "on");
+
+        if (alertNew.equals("on")) {
+            rbNewOn.setChecked(true);
+        }
+        else rbNewOff.setChecked(true);
+
+        if (alertRemind.equals("on")) {
+            rbRemindOn.setChecked(true);
+        }
+        else rbRemindOff.setChecked(true);
+
         Button btnBack = findViewById(R.id.btn_mypage_alert_back);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +49,29 @@ public class MyPageAlertActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        Button btnDone = findViewById(R.id.btn_mypage_alert_done);
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String preferencesNew, preferencesRemind;
+                if (rbNewOn.isChecked()) {
+                    preferencesNew = "on";
+                }
+                else preferencesNew = "off";
+                if (rbRemindOn.isChecked()) {
+                    preferencesRemind = "on";
+                }
+                else preferencesRemind = "off";
+
+                getSharedPreferences("alert", MODE_PRIVATE).edit()
+                        .putString("alertNew", preferencesNew)
+                        .putString("alertRemind", preferencesRemind)
+                        .apply();
+                finish();
+            }
+        });
+
 
 
     }
