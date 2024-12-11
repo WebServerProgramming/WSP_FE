5c621e829c789b1de94884bb4c192c2c8e74becd
nw9233fr2864 <nw9233fr2864@gmail.com>
2024-12-11 20:02:05 +0900

    feat: event and revent handler

--- app/src/main/java/com/example/front/room/RoomReventActivity.java
+++ app/src/main/java/com/example/front/room/RoomReventActivity.java
@@ -1,26 +1,67 @@
 package com.example.front.room;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.front.R;
+import com.example.front.main.MainScheduleDetailAdapter;
+import com.example.front.main.MainScheduleDetailData;
+
+import java.util.ArrayList;
 
 public class RoomReventActivity extends AppCompatActivity {
+
+    // RecyclerView
+    private ArrayList<MainScheduleDetailData> arrayList;
+    private MainScheduleDetailAdapter mainScheduleDetailAdapter;
+    private RecyclerView recyclerView;
+    private GridLayoutManager gridLayoutManager;
+    private static boolean alreadyHappened = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_room_revent);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button btnBack = findViewById(R.id.btn_room_revent_back);
+        Button btnDone = findViewById(R.id.btn_room_revent_done);
+
+        recyclerView = findViewById(R.id.rv_room_revent);
+        gridLayoutManager = new GridLayoutManager(this, 4);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        arrayList = new ArrayList<>();
+        mainScheduleDetailAdapter = new MainScheduleDetailAdapter(arrayList);
+        recyclerView.setAdapter(mainScheduleDetailAdapter);
+        if(alreadyHappened) {
+            MainScheduleDetailData mainScheduleDetailData = new MainScheduleDetailData("ㅊㅊㅎ");
+            arrayList.add(mainScheduleDetailData);
+            mainScheduleDetailAdapter.notifyDataSetChanged();
+        }
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!alreadyHappened) {
+                    alreadyHappened = true;
+                }
+                finish();
+            }
         });
     }
 }
